a245803eb320548f2262aa9c2329f97e1e51669f
OwlRepo <romeoangeles010517@gmail.com>
2021-02-25 19:37:57 +0800

    Logo Localized - Bug Fix

--- src/Assets/Mylogo.tsx
+++ src/Assets/Mylogo.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
+import React from "react";
 
 interface Props {}
 
-const SvgMylogo: React.FC<Props> = (props: Props) => {
+export const SvgMylogo: React.FC<Props> = (props: Props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -86,5 +86,3 @@ const SvgMylogo: React.FC<Props> = (props: Props) => {
     </svg>
   );
 };
-
-export default SvgMylogo;
