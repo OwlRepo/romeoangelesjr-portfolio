f1988f5683bea1a66ae2ec6dabd959f481330b68
OwlRepo <romeoangeles010517@gmail.com>
2021-02-26 00:41:28 +0800

    UI Glitch Bug - Fix

--- src/CustomHooks/useWindowSize.tsx
+++ src/CustomHooks/useWindowSize.tsx
@@ -3,7 +3,10 @@ import React, { useEffect, useState } from "react";
 export const useWindowSize = () => {
   // Initialize state with undefined width/height so server and client renders match
   // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-  const [windowSize, setWindowSize] = useState({});
+  const [windowSize, setWindowSize] = useState({
+    width: 1920,
+    height: 1080,
+  });
 
   useEffect(() => {
     // only execute all the code below in client side
